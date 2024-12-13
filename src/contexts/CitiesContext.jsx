a64e2f5c788623892a56de7e4a44f9badc297b8d
Valentin Ispas <valentin.ispas@nagarro.com>
2024-12-13 15:03:47 +0200

    CRUD operations on Cities

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -38,13 +38,50 @@ function CitiesProvider({ children }) {
     }
   }
 
+  async function createCity(newCity) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities`, {
+        method: 'POST',
+        body: JSON.stringify(newCity),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+      const data = await res.json();
+
+      setCities(cities => [...cities, data]);
+    } catch (error) {
+      alert("There was an error creating city.");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: 'DELETE'
+      });
+
+      setCities(cities => cities.filter(city => city.id !== id));
+    } catch (error) {
+      alert("There was an error deleting city.");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
       value={{
         cities,
         isLoading,
         currentCity,
-        getCity
+        getCity,
+        createCity,
+        deleteCity
       }}
     >
       {children}
